f037ed9c8332d3a72c99d3005940cb147512a957
elsherbene <48767117+elsherbene@users.noreply.github.com>
2019-11-17 20:28:26 +0200

    Add files via upload

---
+++ Controller.java
@@ -0,0 +1,58 @@
+package des;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class Controller {
+
+    public TextField messageTextField;
+    public TextField C0KeyTextField;
+    public TextField D0KeyTextField;
+    public Button encryptButton;
+    public Button decryptButton;
+    public Label headerLabel;
+    public Label encryptedLabel;
+
+    DesCipher desCipher;
+
+    @FXML
+    private void initialize() {
+
+        desCipher = new DesCipher(2);
+
+        encryptButton.setOnMouseClicked(mouseEvent -> handleEncryptButton());
+        decryptButton.setOnMouseClicked(mouseEvent -> handleDecryptedButton());
+    }
+
+    private void handleEncryptButton() {
+        String message = messageTextField.getText();
+        String C0Key = C0KeyTextField.getText();
+        String D0Key = D0KeyTextField.getText();
+
+        desCipher.setKeys(C0Key, D0Key);
+        String encryptedMessage = desCipher.encrypt(message);
+
+        showEncryptedMessage(encryptedMessage);
+    }
+
+    private void handleDecryptedButton() {
+        String encryptedMessage = encryptedLabel.getText();
+        String decryptedMessage = desCipher.decrypt(encryptedMessage);
+        showDecryptedMessage(decryptedMessage);
+    }
+
+    private void showEncryptedMessage(String encryptedMessage) {
+        headerLabel.setVisible(true);
+        encryptedLabel.setText(encryptedMessage);
+        messageTextField.clear();
+    }
+
+    private void showDecryptedMessage(String decryptedMessage) {
+        headerLabel.setVisible(false);
+        messageTextField.setText(decryptedMessage);
+        encryptedLabel.setText("");
+    }
+
+}
---
+++ des.java
@@ -0,0 +1,35 @@
+package des;
+
+import java.io.IOException;
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class des extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+
+        Parent root = FXMLLoader.load(getClass().getResource("design.fxml"));
+        Scene scene = new Scene(root);
+
+        primaryStage.setTitle("Des Algorithm");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
